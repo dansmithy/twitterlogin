a6176c4b3cc9926bf5cb8ce7365f571e93ecd41d
Danny Smith <dansmith1979@gmail.com>
2012-02-13 22:16:37 +0000

    Fixing up files to use scribe

--- src/main/java/com/github/dansmithy/twitterlogin/LoginServlet.java
+++ src/main/java/com/github/dansmithy/twitterlogin/LoginServlet.java
@@ -1,63 +1,17 @@
 package com.github.dansmithy.twitterlogin;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 public class LoginServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        HttpPost method = new HttpPost("https://api.twitter.com/oauth/request_token");
-        
-        TwitterRequest myAccount = new TwitterRequest()
-        	.setConsumerKey("nYOW3tE8e96R7px104ez1w")
-        	.setConsumerSecret(new SecretStore().getConsumerKey())
-        	.setUrl("https://api.twitter.com/oauth/request_token")
-        	.addParam(TwitterRequest.KEY_CALLBACK, "http://twitterlogin.herokuapp.com/twitterCallback");
-        
-        
-        // see https://dev.twitter.com/docs/auth/authorizing-request
-
-        
-//        String authorizationHeader = createAuthorizationHeader(myAccount);
-//        method.addHeader("Authorization", authorizationHeader);
-//        System.out.println(authorizationHeader);
-//        HttpResponse twitResponse = createClientWithProxy("172.16.42.42", 8080).execute(method);
-//        System.out.println(twitResponse.getStatusLine().toString());
-        System.out.println("done");
-    }
-
-
-
-    /**
-     * Setup to create client for use within Nokia (ie. with a proxy set)
-     */
-    private HttpClient createClientWithProxy(String proxyHost, int port) {
-        HttpClient client = new DefaultHttpClient();
-//        HttpHost proxy = new HttpHost(proxyHost, port);
-//        client.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
-        return client;
-    }
-
-    public static void main(String[] args) throws ServletException, IOException {
-        new LoginServlet().service(null, null);
     }
 
 }
---
+++ src/main/java/com/github/dansmithy/twitterlogin/ScribeTest.java
@@ -0,0 +1,65 @@
+package com.github.dansmithy.twitterlogin;
+
+import java.util.Scanner;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.TwitterApi;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+
+public class ScribeTest {
+
+	private static final String PROTECTED_RESOURCE_URL = "https://api.twitter.com/1/statuses/update.json";
+	
+	public static void main(String[] args) {
+		OAuthService service = new ServiceBuilder().provider(TwitterApi.class)
+				.apiKey("nYOW3tE8e96R7px104ez1w")
+				.apiSecret(new SecretStore().getConsumerKey())
+				.callback("http://twitterlogin.herokuapp.com/twitterCallback")
+				.build();
+		Scanner in = new Scanner(System.in);
+
+		System.out.println("=== Twitter's OAuth Workflow ===");
+		System.out.println();
+
+		// Obtain the Request Token
+		System.out.println("Fetching the Request Token...");
+		Token requestToken = service.getRequestToken();
+		System.out.println("Got the Request Token!");
+		System.out.println();
+
+		System.out.println("Now go and authorize Scribe here:");
+		System.out.println(service.getAuthorizationUrl(requestToken));
+		System.out.println("And paste the verifier here");
+		System.out.print(">>");
+		Verifier verifier = new Verifier(in.nextLine());
+		System.out.println();
+
+		// Trade the Request Token and Verfier for the Access Token
+		System.out.println("Trading the Request Token for an Access Token...");
+		Token accessToken = service.getAccessToken(requestToken, verifier);
+		System.out.println("Got the Access Token!");
+		System.out.println("(if your curious it looks like this: "
+				+ accessToken + " )");
+		System.out.println();
+
+		// Now let's go and ask for a protected resource!
+		System.out.println("Now we're going to access a protected resource...");
+		OAuthRequest request = new OAuthRequest(Verb.POST,
+				PROTECTED_RESOURCE_URL);
+		request.addBodyParameter("status", "this is sparta! *");
+		service.signRequest(accessToken, request);
+		Response response = request.send();
+		System.out.println("Got it! Lets see what we found...");
+		System.out.println();
+		System.out.println(response.getBody());
+
+		System.out.println();
+		System.out
+				.println("Thats it man! Go and build something awesome with Scribe! :)");
+	}
+}
