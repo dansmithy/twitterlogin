7ced9b49579ba497e8f6f59725e806cb1017285a
Danny Smith <dansmith1979@gmail.com>
2012-02-18 17:41:02 +0000

    Completed acceptance test

--- src/test/java/com/github/dansmithy/twitterlogin/TwitterLoginAT.java
+++ src/test/java/com/github/dansmithy/twitterlogin/TwitterLoginAT.java
@@ -17,22 +17,30 @@ public class TwitterLoginAT {
 
 	public ClientDriverRule clientDriver = new ClientDriverRule(ATUtils.getRestDriverPort());
 	
+	private static final String BASE_URL = "http://localhost:8086";
 	@Test
 	public void testAuth() {
 		
-		String token = "test_token";
-		String tokenSecret = "test_token_secret";
-		String tokenVerifier = "test_token_verifier";
-		String authTokenReponseBody = String.format("oauth_token=%s&oauth_token_secret=%s&oauth_callback_confirmed=true", token, tokenSecret);
-		clientDriver.addExpectation(onRequestTo("/oauth/request_token").withMethod(Method.POST), giveResponse(authTokenReponseBody).withStatus(HttpURLConnection.HTTP_OK));
-		Response requestTokenResponse = get("http://localhost:8086/ws/auth/authToken");
+		String requestToken = "request_token";
+		String requestTokenSecret = "request_token_secret";
+		String accessToken = "access_token";
+		String accessTokenSecret = "access_token_secret";
+		String accessTokenVerifier = "access_token_verifier";
+		String userId = "user_id";
+		String username = "username";
+		
+		String requestTokenReponseBody = String.format("oauth_token=%s&oauth_token_secret=%s&oauth_callback_confirmed=true", requestToken, requestTokenSecret);
+		clientDriver.addExpectation(onRequestTo("/oauth/request_token").withMethod(Method.POST), giveResponse(requestTokenReponseBody).withStatus(HttpURLConnection.HTTP_OK));
+		Response requestTokenResponse = get(BASE_URL + "/ws/auth/authToken");
 		
 		Assert.assertThat(requestTokenResponse.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_MOVED_TEMP)));
 		
 		String location = requestTokenResponse.getHeader("location").getValue();
-		Assert.assertThat(location, is(equalTo(String.format("http://localhost:%d/oauth/authenticate?oauth_token=%s", ATUtils.getRestDriverPort(), token))));
+		Assert.assertThat(location, is(equalTo(String.format("http://localhost:%d/oauth/authenticate?oauth_token=%s", ATUtils.getRestDriverPort(), requestToken))));
 		
-		Response validateUserResponse = get(String.format("http://localhost:8086/ws/auth/authValidate?oauth_token=%s&oauth_verifier=%s", token, tokenVerifier));
+		String accessTokenReponseBody = String.format("oauth_token=%s&oauth_token_secret=%s&user_id=%s&screen_name=%s", accessToken, accessTokenSecret, userId, username);
+		clientDriver.addExpectation(onRequestTo("/oauth/access_token").withMethod(Method.POST), giveResponse(accessTokenReponseBody).withStatus(HttpURLConnection.HTTP_OK));
+		Response validateUserResponse = get(String.format(BASE_URL + "/ws/auth/authValidate?oauth_token=%s&oauth_verifier=%s", requestToken, accessTokenVerifier));
 		Assert.assertThat(validateUserResponse.getStatusCode(), is(equalTo(HttpURLConnection.HTTP_MOVED_TEMP)));
 		
 	}
